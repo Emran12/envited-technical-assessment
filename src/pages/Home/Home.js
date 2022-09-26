2f4503defe99ff8fbe50fba38efa8dcf60a9527b
Emran12 <emran6452@gmail.com>
2022-09-26 09:43:07 +0600

    landing page added

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,7 +1,27 @@
 import React from "react";
+import LandingPageImage from "../../images/LandingPageImage.svg";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="ml-[170px] mt-[100px]">
+      <div>
+        <img src={LandingPageImage} alt="" />
+      </div>
+      <div>
+        <h1 className=" text-[64px] leading-[64px]">
+          Imagine If
+          <span className="bg-gradient-[#8456EC 100% #E87BF8  100%]">
+            Snapchat
+          </span>
+          <span>had events.</span>
+        </h1>
+        <p>
+          Easily host and share events with your friends across any social
+          media.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
